4aeb1fd84ae9773122454b00fbc0443cd4c955b0
spazdrum <spazdrum@gmail.com>
2022-07-28 14:04:04 -0400

    todo list functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import { ShowTodoList } from "./components/showTodoList";
+import { CreateTodo } from "./components/createTodo";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-contents">
+      <BrowserRouter>
+        <Route exact path="/" element={<ShowTodoList />} />
+        <Route path="/create-todo" component={<CreateTodo />} />
+      </BrowserRouter>
     </div>
   );
 }
